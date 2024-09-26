6a32aa95086297ef681b992b2acd9a9531cccd2d
tmarkec <105808713+tmarkec@users.noreply.github.com>
2024-09-26 10:38:55 +0000

    add placeholder image & post.html

--- kudoboard/settings.py
+++ kudoboard/settings.py
@@ -24,13 +24,17 @@ if os.path.exists('env.py'):
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'KM123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
+
 
 ALLOWED_HOSTS = [
-    'https://vigilant-guacamole-q5w59qgv9qwfxrj5-8000.app.github.dev/'
+    'https://vigilant-guacamole-q5w59qgv9qwfxrj5-8000.app.github.dev/',
+    'localhost'
 ]
 
 
